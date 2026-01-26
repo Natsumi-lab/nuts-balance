790dff20aec5bbbfbc973f0920a9a7c31ff1a2e9
Natsumi-lab <imustan81@gmail.com>
2026-01-26 20:56:39 +0900

    delete card from TodayScoreCard

--- src/app/(protected)/app/_components/NutCheckList.tsx
+++ src/app/(protected)/app/_components/NutCheckList.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import type { Nut, ActionResult } from "@/lib/types";
 import { upsertDailyLog } from "../actions";
+import TodayScoreCard from "./TodayScoreCard";
 
 /**
  * ナッツチェックリストコンポーネントのプロパティ型
